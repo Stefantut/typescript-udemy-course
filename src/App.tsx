be59e19f26f31fee31016830c2f7622444a20c20
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-09-23 23:45:32 +0100

    Tested typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function sum(a: number, b: number): number {
+    return a + b;
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {sum(2, 15)}
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
