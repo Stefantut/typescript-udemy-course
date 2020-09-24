283da57fa2a4ddf88203e7d972e996655575aecb
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-09-24 22:56:19 +0100

    Added handle form submit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Todo App built following this course:
+https://www.udemy.com/course/typescript-with-react-hooks-and-context
 
 ## Available Scripts
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 
+type FormElem = React.FormEvent<HTMLFormElement>;
+
 function App(): JSX.Element {
   const [value, setValue] = useState<string>("");
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    setValue("");
+  };
+
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <form>
-        <input type="text" required />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Add Todo</button>
       </form>
     </div>
